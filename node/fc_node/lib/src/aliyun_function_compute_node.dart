d6f3874b8fbef35c9c70738fa00475f53a7c7470
Alexandre Roux <alex@tekartik.com>
2023-03-12 16:13:26 +0100

    fix dart 3 compile conflict

--- node/fc_node/lib/src/aliyun_function_compute_node.dart
+++ node/fc_node/lib/src/aliyun_function_compute_node.dart
@@ -1,6 +1,5 @@
 import 'dart:js';
-import 'dart:js_util';
-
+import 'dart:js_util' as jsu;
 import 'package:node_interop/node_interop.dart' as node;
 import 'package:tekartik_aliyun_fc/fc_api.dart';
 import 'package:tekartik_aliyun_fc_node/fc_interop.dart';
@@ -8,7 +7,7 @@ import 'package:tekartik_aliyun_fc_node/fc_interop.dart';
 class AliyunFunctionComputeNode implements AliyunFunctionCompute {
   @override
   void exportHttpHandler(FcHttpHandler handler, {String name = 'handler'}) {
-    setProperty(node.exports as Object, name, allowInterop(
+    jsu.setProperty(node.exports as Object, name, allowInterop(
         (HttpReqJs req, HttpResponseJs resp, HttpContextJs context) async {
       var httpReq = FcHttpRequestNode(req);
       var httpResp = FcHttpResponseNode(resp);
